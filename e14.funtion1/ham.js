142a4a8103b6c18f5475a99f8cc4a3ff82338bb0
Your Namethanhhoai1997 <hoain5990@gmail.com>
2023-06-12 21:01:29 +0700

    UpFuntion

---
+++ e14.funtion1/ham.js
@@ -0,0 +1,12 @@
+function  square(number) {
+    var result = number*number
+    return result
+
+
+}
+function calculate() {
+    var input = document.getElementById("input").value;
+    var result = square(input);
+    document.getElementById('result').innerHTML = result
+
+}
